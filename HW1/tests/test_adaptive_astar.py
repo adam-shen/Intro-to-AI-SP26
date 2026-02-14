bd51e68a7e6dcce9c6ffcf6951db2ab268c6c236
Adam Elshanawany <113874468+adam-shen@users.noreply.github.com>
2026-02-14 17:48:27 +0000

    creating new branch instead of deleting and readding stuff

--- HW1/tests/test_adaptive_astar.py
+++ HW1/tests/test_adaptive_astar.py
@@ -1,6 +1,7 @@
 # test_adaptive_astar.py
 from src.grid import Grid
 from src.adaptive_astar import adaptive_forward_astar
+from src.visualize import print_grid_window  
 
 true_grid = Grid.from_file("data/grid_002.txt")
 
@@ -12,3 +13,7 @@ path = adaptive_forward_astar(true_grid, start, goal)
 print("Repeated Adaptive A* path:")
 print(path)
 print("Path length:", None if path is None else len(path))
+
+# visualize in terminal
+print("\nGrid window with path:")
+print_grid_window(true_grid, path=path, start=start, goal=goal, padding=2)
